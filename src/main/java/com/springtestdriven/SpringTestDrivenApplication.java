31be996ff8d5e77195b72003512106836df97410
Athirson Silva <84593887+AthirsonSilva@users.noreply.github.com>
2023-04-19 20:05:22 -0300

    initial commit

---
+++ src/main/java/com/springtestdriven/SpringTestDrivenApplication.java
@@ -0,0 +1,13 @@
+package com.springtestdriven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestDrivenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestDrivenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springtestdriven/SpringTestDrivenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtestdriven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestDrivenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
