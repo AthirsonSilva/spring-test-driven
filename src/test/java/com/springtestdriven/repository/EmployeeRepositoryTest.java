f92f75e001704c44465b357783751f1c2060aea9
Athirson Silva <84593887+AthirsonSilva@users.noreply.github.com>
2023-04-21 15:41:13 -0300

    test: find all and find by id test cases

--- src/test/java/com/springtestdriven/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/springtestdriven/repository/EmployeeRepositoryTest.java
@@ -3,6 +3,7 @@ package com.springtestdriven.repository;
 import com.github.javafaker.Faker;
 import com.springtestdriven.entity.EmployeeEntity;
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,25 +11,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
 @Log4j2
 class EmployeeRepositoryTest {
-    @Autowired private EmployeeRepository employeeRepository;
-    @Autowired private Faker faker;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Autowired
+    private Faker faker;
+
+    @AfterEach
+    void tearDown() {
+        employeeRepository.deleteAll();
+    }
 
     @Test
-    @DisplayName("Test create employee operation")
+    @DisplayName("Given employee object, when save is called, then save employee object")
     void create() {
         // given - precondition
-        EmployeeEntity employeeEntity = EmployeeEntity.builder()
-                .firstName(faker.name().firstName())
-                .lastName(faker.name().lastName())
-                .email(faker.internet().emailAddress())
-                .build();
+        EmployeeEntity employeeEntity = createEmployeeEntity();
 
         // when - action
         EmployeeEntity savedEmployee = employeeRepository.save(employeeEntity);
@@ -41,6 +44,54 @@ class EmployeeRepositoryTest {
     }
 
     @Test
-    void findAll() {
+    @DisplayName("Given employees in the database, when findAll is called, then return list of employees")
+    public void findAll() {
+        // given - precondition
+        for (int i = 0; i < 5; i++) {
+            employeeRepository.save(createEmployeeEntity());
+        }
+
+        // when - action
+        Iterable<EmployeeEntity> employeeEntities = employeeRepository.findAll();
+
+        // then - assertion
+        assertEquals(5, employeeEntities.spliterator().getExactSizeIfKnown());
+        employeeEntities.forEach(employeeEntity -> {
+            assertNotNull(employeeEntity.getId());
+            assertNotNull(employeeEntity.getCreatedAt());
+            assertNull(employeeEntity.getUpdatedAt());
+        });
+
+        log.info("employeeEntities = {}", employeeEntities);
+    }
+
+    @Test
+    @DisplayName("Given an employee ID, when findById is called, then return employee object")
+    public void find() {
+        // given - precondition
+        EmployeeEntity employeeEntity = createEmployeeEntity();
+        EmployeeEntity savedEmployee = employeeRepository.save(employeeEntity);
+
+        // when - action
+        EmployeeEntity foundEmployee = employeeRepository.findById(savedEmployee.getId()).orElse(null);
+
+        // then - assertion
+        assertNotNull(foundEmployee);
+        assertEquals(savedEmployee.getId(), foundEmployee.getId());
+        assertEquals(savedEmployee.getFirstName(), foundEmployee.getFirstName());
+
+        log.info("foundEmployee = {}", foundEmployee);
+    }
+
+    /**
+     * @return EmployeeEntity
+     * @implNote Create EmployeeEntity object with random data using Faker
+     */
+    private EmployeeEntity createEmployeeEntity() {
+        return EmployeeEntity.builder()
+                .firstName(faker.name().firstName())
+                .lastName(faker.name().lastName())
+                .email(faker.internet().emailAddress())
+                .build();
     }
 }
